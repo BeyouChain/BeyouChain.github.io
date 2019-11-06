a4a1c3c3b53877a3b8e2f240e32e75520c6e89f4
张凌哲 <zhanglingzhe@bytedance.com>
2019-11-06 22:11:08 +0800

    del

--- static/js/index.50a3a1ef6989afeb1643.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([3],{"2wEm":function(e,n,p){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),new(p("7+uW").a)({el:"#app"})}},["2wEm"]);
-//# sourceMappingURL=index.50a3a1ef6989afeb1643.js.map
